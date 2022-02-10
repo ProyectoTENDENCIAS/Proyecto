27fbcb13d69f3ff8ce4eaa8f5407273d4d792c6d
ProyectoTENDENCIAS <99391919+ProyectoTENDENCIAS@users.noreply.github.com>
2022-02-10 04:15:47 -0500

    Add files via upload

---
+++ ConsultaapiApplication.kt
@@ -0,0 +1,11 @@
+package com.mauricionaula.consultaapi.ConsultaapiApplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConsultaapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConsultaapiApplication>(*args)
+}
